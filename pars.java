bd4d548a8d08ecaf6fee971c27e98cdd71f62293
Ivan1337 <79834829+Ivan881914@users.noreply.github.com>
2022-11-13 17:07:50 +0800

    Update pars.java

--- pars.java
+++ pars.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
+import org.jsoup.*;
+import java.io.IOException;
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
+    public Main(){
+        super("Погода на неделю Иркутск"); //Заголовок окна
+        setBounds(100, 100, 200, 200); //Если не выставить
+        //размер и положение
+        //то окно будет мелкое и незаметное
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //это нужно для того чтобы при
+        //закрытии окна закрывалась и программа,
+        //иначе она останется висеть в процессах
+    }
+    public static void main(String[] args) throws IOException {
         var document = Jsoup.connect("https://yandex.ru/pogoda/irkutsk?lat=52.28959&lon=104.280608").get();
         //System.out.println(document);
         var dayElement = document.select("div.forecast-briefly__name");
@@ -20,6 +33,8 @@ public class Main {
         System.out.println(temp_words[1]);
         System.out.println(temp_words[2]);
         System.out.println(temp_words[3]);
-        System.out.println("Hello world!");
+
+        Main app = new Main(); //Создаем экземпляр нашего приложения
+        app.setVisible(true); //С этого момента приложение запущено!
     }
 }
